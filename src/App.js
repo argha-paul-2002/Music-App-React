d7ef34e8c65b9271a30c79253e63316c5ee42f60
Argha Paul <arghapaul002@gmail.com>
2023-07-29 00:35:30 +0530

    Header designed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="App text-white overflow-hidden">
+      <Header />
     </div>
   );
 }
