1467bd2a56d25841d0ce0bde940fbb9de5e728cc
Argha Paul <arghapaul002@gmail.com>
2023-07-29 10:38:31 +0530

    Experience and search section designed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Experience from './components/Experience';
 import Header from './components/Header';
+import Hero from './components/Hero';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App text-white overflow-hidden">
       <Header />
+      <Hero />
+      <Experience />
+      <Search />
     </div>
   );
 }
